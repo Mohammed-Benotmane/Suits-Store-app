6b905fc77bd2efbb10c768555f26c916d038be9e
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-05-07 19:18:28 +0200

    add screens: profile, home, store

---
+++ lib/home.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:profiledarktheme/main.dart';
+import 'package:profiledarktheme/profile.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Profile(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.store),
+            title: Text('Store'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Profile'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color(0xFF743a34),
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ lib/store.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Store extends StatefulWidget {
+  @override
+  _StoreState createState() => _StoreState();
+}
+
+class _StoreState extends State<Store> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Profile',
+          style: TextStyle(color: Colors.grey[300]),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[900],
+      ),
+    );
+  }
+}
